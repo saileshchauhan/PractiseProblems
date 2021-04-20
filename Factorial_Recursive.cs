8f7821053de1a6b4c5d8a5eacef7f5362acfe1c7
saileshchauhan <chauhansailesh68@gmail.com>
2021-04-20 15:20:17 +0530

    first commit

---
+++ Factorial_Recursive.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FellowshipProblem
+{
+    class Factorial_Recursive
+    {
+        public void Factorial()
+        {
+         
+                Console.WriteLine("Enter a number");
+                int number = Convert.ToInt32(Console.ReadLine());
+                long numberFactorial = Recursive(number);
+                Console.WriteLine("{0} factorial is {1}", number, numberFactorial);
+                Console.ReadKey();
+
+                static long Recursive(int number)
+                {
+                    if (number == 0)
+                    {
+                        return 1;
+                    }
+                    return number * Recursive(number - 1);
+                }
+           
+        }
+    }
+}
