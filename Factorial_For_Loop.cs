66656eb7eb03610985ed5c97eb6e8d1342802e35
saileshchauhan <chauhansailesh68@gmail.com>
2021-04-20 15:26:48 +0530

    Addition branch

---
+++ Factorial_For_Loop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FellowshipProblem
+{
+    class Factorial_For_Loop
+    {
+        public void FactorialForLoop()
+        {
+            Console.WriteLine("Enter a number For loop ");
+            int number=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" {0} Factorial is {1} ",number,ForLoop(number));
+        }
+        static long ForLoop(int number)
+
+        {   int fact = 1;
+            if (number == 0)
+            {
+                return 1;
+            }
+            for (int i = number; i > 0; i--)
+            {
+                  fact *= i;
+            }
+            return fact;
+        }
+    }   
+
+}
